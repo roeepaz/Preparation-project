be6e97932127586f98c65ce603a5f10c0ecb937f
roeepaz <roeepaz333531@gmail.com>
2024-08-15 19:05:23 +0300

    my changes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, jsonify
+
+from flask import Flask, render_template_string, jsonify
+
+import pandas as pd
+
 
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({'message': 'Hello from Flask!'})
+    return jsonify({'message': 'we are up!'})
+
+
+@app.route('/')
+def show_dataframe():
+    df = pd.read_csv('tasks.csv')
+
+    # Convert DataFrame to HTML
+    html_table = df.to_html(classes='table table-striped', index=False)
+    
+    # Render the HTML template
+    return render_template_string('''
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            </head>
+            <body>
+                <div class="container">
+                    <h1>DataFrame Table</h1>
+                    {{ table | safe }}
+                </div>
+            </body>
+        </html>
+    ''', table=html_table)
 
 if __name__ == '__main__':
     app.run(debug=True)
